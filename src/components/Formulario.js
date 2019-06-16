c8d8d3e06f79c4c52c1d12e3cd336c87a177c3a4
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-06-16 15:41:03 -0500

    Add Form

---
+++ src/components/Formulario.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Formulario(){
+  const handleChange = e =>{
+    // Camniar State
+  }
+
+
+  return (
+    <form>
+      <div className="input-field col s12">
+        <input 
+        type="text"
+        name="ciudad"
+        id = "ciudad"
+        onChange = {handleChange}
+        />
+        <label htmlFor ="ciudad">Ciudad:</label>
+      </div>
+
+      <div className="input-field col s12">
+        <select onChange={handleChange} name ="pais">
+          <option value="">Selecciona un País</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
+        </select>
+      </div>
+      <div className="input-field col s12">
+        <input type ="submit" className="waves-effect waves-light btn-large btn-block yellow accent-4" value="Buscar Clima"/>
+        </div>
+      
+    </form>
+  )
+
+}
+
+export default Formulario
